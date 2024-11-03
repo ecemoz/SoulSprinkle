091a88ba9a1a935a202787af4fc0d03d639f9998
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-03 17:14:14 +0300

    Task Tag finished.

--- src/main/java/com/yildiz/soulsprinkle/model/TaskTag.java
+++ src/main/java/com/yildiz/soulsprinkle/model/TaskTag.java
@@ -1,4 +1,29 @@
 package com.yildiz.soulsprinkle.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name ="task_tags")
+
 public class TaskTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @ManyToOne
+    @JoinColumn( name ="task_id",nullable = false)
+    private Task task;
+
+    private String tagName;
+
+    public TaskTag( String tagName, Task task) {
+        this.tagName = tagName;
+        this.task = task;
+    }
+
+    public TaskTag() {
+    }
 }
