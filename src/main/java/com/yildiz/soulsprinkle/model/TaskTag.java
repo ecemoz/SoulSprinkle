fa245b574f8494629d1c931e75056e4bc1985caf
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-03 17:37:13 +0300

    Task tag updated.

--- src/main/java/com/yildiz/soulsprinkle/model/TaskTag.java
+++ src/main/java/com/yildiz/soulsprinkle/model/TaskTag.java
@@ -11,6 +11,18 @@ public class TaskTag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id ;
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+}
+
+
+
 
 
