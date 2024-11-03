92e685e1f8f55c8c2d79a7201112ac1b5bd36f43
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-03 17:24:21 +0300

    tag model class finished.

--- src/main/java/com/yildiz/soulsprinkle/model/Tag.java
+++ src/main/java/com/yildiz/soulsprinkle/model/Tag.java
@@ -1,4 +1,21 @@
 package com.yildiz.soulsprinkle.model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "tags")
 public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "tag")
+    private Set<TaskTag> taskTags;
 }
--- src/main/java/com/yildiz/soulsprinkle/model/TaskTag.java
+++ src/main/java/com/yildiz/soulsprinkle/model/TaskTag.java
@@ -13,17 +13,4 @@ public class TaskTag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
 
-    @ManyToOne
-    @JoinColumn( name ="task_id",nullable = false)
-    private Task task;
 
-    private String tagName;
-
-    public TaskTag( String tagName, Task task) {
-        this.tagName = tagName;
-        this.task = task;
-    }
-
-    public TaskTag() {
-    }
-}
