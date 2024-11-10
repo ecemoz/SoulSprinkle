2a656edd226a1d097e651c1618ca513bc52fd446
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-10 21:39:28 +0300

    Task Repository finished.

--- src/main/java/com/yildiz/soulsprinkle/repository/TaskRepository.java
+++ src/main/java/com/yildiz/soulsprinkle/repository/TaskRepository.java
@@ -1,4 +1,24 @@
 package com.yildiz.soulsprinkle.repository;
 
-public class TaskRepository {
+import com.yildiz.soulsprinkle.model.Task;
+import com.yildiz.soulsprinkle.model.preferences.TaskStatusPreference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository {
+
+    List<Task>findByCreatorId(Long creatorId);
+
+    List<Task>findByTaskStatusPreference(TaskStatusPreference taskStatusPreference);
+
+    List<Task>findByDueDateBefore(LocalDateTime date);
+
+    List<Task>findByTitleContaining(String title);
+
+    List<Task>findByTaskStatusPreferenceAndAssigneesId(TaskStatusPreference taskStatusPreference, Long assigneeId);
 }
