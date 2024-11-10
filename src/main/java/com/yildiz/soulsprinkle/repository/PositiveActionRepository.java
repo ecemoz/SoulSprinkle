79c0930a579cf6221efcdc12f88ffa073ba261ff
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-10 22:23:28 +0300

    update positive action repo

--- src/main/java/com/yildiz/soulsprinkle/repository/PositiveActionRepository.java
+++ src/main/java/com/yildiz/soulsprinkle/repository/PositiveActionRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface PositiveActionRepository extends JpaRepository <PositiveAction,Long>{
 
-    Optional<PositiveAction> findByTaskId(Long taskId);
+    Optional<PositiveAction> findByReleatedTasksId(Long taskId);
 }
