8683503ef748868a05022b5549edcecf5e5fd7c1
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-10 20:28:42 +0300

    positive action  finished.

--- src/main/java/com/yildiz/soulsprinkle/model/PositiveAction.java
+++ src/main/java/com/yildiz/soulsprinkle/model/PositiveAction.java
@@ -1,4 +1,30 @@
 package com.yildiz.soulsprinkle.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PositiveAction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id",nullable = false, unique = true)
+    private Task relatedTasks;
 }
