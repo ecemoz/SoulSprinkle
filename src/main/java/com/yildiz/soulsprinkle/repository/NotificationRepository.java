8f26e00f96d34de367355fc93f41ee015d8d7dba
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-10 22:13:08 +0300

    Notification Repository finished

--- src/main/java/com/yildiz/soulsprinkle/repository/NotificationRepository.java
+++ src/main/java/com/yildiz/soulsprinkle/repository/NotificationRepository.java
@@ -1,4 +1,16 @@
 package com.yildiz.soulsprinkle.repository;
 
-public class NotificationRepository {
+import com.yildiz.soulsprinkle.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository <Notification,Long> {
+
+    List<Notification>findByUserId(Long userId);
+
+    List<Notification>findByTaskId(Long taskId);
+
+    List<Notification>findByUserIdAndReadStatusFalse(Long userId);
 }
