b6b03706109bbfc780a951aadcd2c80183d30676
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-10 21:28:54 +0300

    UserRepository finished.

---
+++ src/main/java/com/yildiz/soulsprinkle/repository/PositiveActionRepository.java
@@ -0,0 +1,4 @@
+package com.yildiz.soulsprinkle.repository;
+
+public class PositiveActionRepository {
+}
---
+++ src/main/java/com/yildiz/soulsprinkle/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.yildiz.soulsprinkle.repository;
+
+import com.yildiz.soulsprinkle.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository  extends JpaRepository {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User>findByFirstnameAndLastname(String firstname, String lastname);
+
+    Optional<User>findByEmailAndUsername(String email, String username);
+
+    List<User>findByPictureUrlIsNotNull();
+}
