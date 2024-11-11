5ed87bfa3176e391f929ee5882f36b866690ef94
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-11 19:24:55 +0300

    notification service finished.

---
+++ src/main/java/com/yildiz/soulsprinkle/service/NotificationService.java
@@ -0,0 +1,39 @@
+package com.yildiz.soulsprinkle.service;
+
+import com.yildiz.soulsprinkle.model.Notification;
+import com.yildiz.soulsprinkle.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    @Autowired
+    private NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    public Optional<Notification>getNotificationById(long id) {
+        return notificationRepository.findById(id);
+    }
+
+    public List<Notification> getNotificationsByTaskId(Long taskId) {
+        return notificationRepository.findByTaskId(taskId);
+    }
+
+    public  Notification saveNotification(Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    public void deleteNotificationById(Long id) {
+        if (notificationRepository.existsById(id)) {
+            notificationRepository.deleteById(id);
+        }else {
+            throw new RuntimeException("Notification not found");
+        }
+    }
+}
